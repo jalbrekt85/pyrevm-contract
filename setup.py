fdc93e410b50c444e931bda0ab191d781b58ae9c
jalbrekt85 <jcalbrecht85@gmail.com>
2023-10-11 19:26:49 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pyrevm_contract",
-    version="0.1.8",
+    version="0.1.9",
     author="jalbrekt85",
     author_email="jcalbrecht85@gmail.com",
     description="Minimal Brownie like contract wrapper for Pyrevm",
